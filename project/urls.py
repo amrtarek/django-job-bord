f62331f7d14ec663079e6fc0e3ee3137918d232d
amr_tarek <amrtarek@paymob.com>
2021-09-11 19:21:29 +0200

    add basic 4 apps in our project

--- project/urls.py
+++ project/urls.py
@@ -1,4 +1,4 @@
-"""project URL Configuration
+-	Python mange.py startapp """project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
